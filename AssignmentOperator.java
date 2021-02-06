5c9eacaca82e1f098ffa46a6cb8f26ddfdbd3a19
Shahriar Momen <66987174+shahriar-swe@users.noreply.github.com>
2021-02-06 05:17:05 -0800

    3rd Commit

---
+++ AssignmentOperator.java
@@ -0,0 +1,30 @@
+
+package basicjava;
+
+/**
+ *
+ * @author shahr
+ */
+public class AssignmentOperator {
+    
+    public static void main(String[] args) {
+        int x = 3,y = 2;
+        
+        x+=y;//x = x + y = 3 + 2 = 5
+        System.out.println("x = "+x);
+        
+        x-=y;//x = x - y = 5 - 2 = 3
+        System.out.println("x = "+x);
+        
+        x*=y;//x = x * y = 3 * 2 = 6
+        System.out.println("x = "+x);
+        
+        x/=y;//x = x / y = 6 / 2 = 3
+        System.out.println("x = "+x);
+        
+        x%=y;//x = x % y = 5 % 2 = 3
+        System.out.println("x = "+x);
+       
+    }
+    
+}
